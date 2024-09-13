c1661807774fc2f1e6ac2ae72da1e4a95f266df0
Mutaf-Volodymyr <mutaf.dev@gmail.com>
2024-09-13 08:57:39 +0200

    изменено:      Python/something/2024_09_10_practicum.py, SQL/Homework/HW_6.sql

--- SQL/Homework/HW_6.sql
+++ SQL/Homework/HW_6.sql
@@ -23,13 +23,29 @@ from city as ci
 inner join country as co
 on ci.CountryCode = co.Code;
 
-# Выведите список городов в South Africa
+# Выведите список городов в South Africa (если речь о регионе)
 select
     ci.Name as city
 from city as ci
 inner join country as co
 on ci.CountryCode = co.Code
 and co.Region = 'Southern Africa';
+
+# 4.  ИЛИ Выведите список городов в South Africa
+# (если речь о стране ЮАР)
+
+select Name, CountryCode
+from world.city
+where CountryCode = (
+    select Code
+    from world.country
+    where Name = 'South Africa');
+
+-- Вариант Лены
+-- 4. Выведите список городов в South Africa
+SELECT city.Name AS city_name, country.Name FROM city
+JOIN country ON country.code = city.CountryCode
+AND country.Name = 'South Africa';
 
 # Выведите список стран с названиями столиц.
 # Подсказка: в таблице country есть поле Capital,
@@ -50,6 +66,13 @@ select
 from world.country as w
 inner join world.city c
     on w.Capital = c.ID;
+
+
+-- Или вариант Лены
+-- 6. Измените запрос 4 таким образом, чтобы выводилось население в столице.
+SELECT country.Name, city.Name, city.Population FROM country
+JOIN city ON city.ID = country.Capital
+AND country.Name = 'South Africa';
 
 # Напишите запрос, который возвращает название столицы United States
 select
@@ -78,3 +101,9 @@ select
 from locations as l
 inner join countries as c
 on l.country_id = c.country_id;
+
+
+
+
+
+
