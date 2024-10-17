dc1249476445c19502cf3f4b209b3bdc4ba1fd73
Mutaf-Volodymyr <mutaf.dev@gmail.com>
2024-10-17 13:57:32 +0200

    новый файл:    Python/Homework/123.txt 	новый файл:    Python/Homework/HW_21.py 	новый файл:    Python/Homework/HW_22.py 	изменено:      SQL/.idea/sqldialects.xml

---
+++ Python/Homework/HW_21.py
@@ -0,0 +1,49 @@
+# 1. Напишите программу, которая подсчитывает количество вхождений каждого слова в тексте и
+# выводит на экран наиболее часто встречающиеся слова. Для решения задачи используйте класс
+# Counter из модуля collections. Создайте функцию count_words, которая принимает текст в качестве
+# аргумента и возвращает словарь с количеством вхождений каждого слова. Выведите наиболее
+# часто встречающиеся слова и их количество.
+
+# from collections import Counter
+# from string import punctuation
+#
+# text = [i.lower().strip(punctuation) for i in input().split()]
+# res = Counter(text)
+# for k, v in res.most_common():
+#     print(f'слово "{k}" встречается: {v} раз(а)')
+
+
+
+
+# 2. Напишите программу, которая создает именованный кортеж Person для хранения информации о человеке,
+# включающий поля name, age и city. Создайте список объектов Person и выведите информацию о каждом человеке на экран.
+
+# from collections import namedtuple
+# Person = namedtuple('Person', ['name', 'age', 'city'], defaults=('NoName', 'NoAge', 'NoCity'))
+#
+# alice = Person('Alice', 25, 'New York')
+# bob = Person('Bob', 30, 'London')
+# carol = Person('Carol', 35)
+# someone = Person()
+#
+# for i in [alice, bob, carol, someone]:
+#     print(f'{i.name}s {i.age} years old.')
+
+
+# 3. Напишите программу, которая принимает словарь от пользователя и ключ, и возвращает значение для указанного
+# ключа с использованием метода get или setdefault. Создайте функцию get_value_from_dict, которая принимает словарь
+# и ключ в качестве аргументов, и возвращает значение для указанного ключа, используя метод get или setdefault
+# в зависимости от выбранного варианта. Выведите полученное значение на экран.
+#
+
+# def get_value_from_dict(di:dict, k:str|int, flag=True):
+#     if flag:
+#         return di.get(k, 'нет ключа')
+#     else:
+#         return di.setdefault(k, 'нет значения')
+#
+# my_dict = {'apple': 5, 'banana': 6, 'cherry': 7}
+# print(get_value_from_dict(my_dict, 'banana'))
+# print(get_value_from_dict(my_dict, 'bananaAAAAA'))
+# print(get_value_from_dict(my_dict, 'bananaAAAAA', False))
+# print(my_dict)
---
+++ Python/Homework/HW_22.py
@@ -0,0 +1,55 @@
+# 1. Напишите программу, которая открывает файл, считывает из него два числа и выполняет операцию их деления.
+# Если число отрицательное, выбросите исключение ValueError с сообщением "Число должно быть положительным".
+# Обработайте исключение и выведите соответствующее сообщение.
+
+from time import sleep
+try:
+    with open('123.txt', 'r', encoding='utf-8') as f:
+        for line in f:
+            a, b = [int(i) for i in line.strip().split(' ')]
+            if a < 0 or b < 0:
+                raise ValueError
+            print(a / b)
+except FileNotFoundError:
+    print('нет такого файла')
+except ValueError:
+    print('одно из чисел имеет отрицательное значение')
+except ZeroDivisionError:
+    print('Только не деление на ноль!!!!')
+else:
+    print('Процесс успешно завершен')
+finally:
+    print('Ваш компьютер взорвется через:')
+    for i in [5, 4, 3, 2, 1 , 0.5, 0.25]:
+        print(i)
+        sleep(1)
+    print('БАБАХ')
+
+
+
+
+# 2. Напишите программу, которая открывает файл, считывает его содержимое и выполняет операции над числами в файле.
+# Обработайте возможные исключения при открытии файла (FileNotFoundError) и при выполнении операций над числами
+# (ValueError, ZeroDivisionError). Используйте конструкцию try-except-finally для обработки исключений
+# и закрытия файла в блоке finally.
+
+try:
+    file = open('123.txt', 'r', encoding='utf-8')
+    for line in f:
+        a, b = [int(i) for i in line.strip().split(' ')]
+        if a < 0 or b < 0:
+            raise ValueError
+        print(a / b)
+except FileNotFoundError:
+    print('нет такого файла')
+except ValueError:
+    print('одно из чисел имеет отрицательное значение')
+except ZeroDivisionError:
+    print('Только не деление на ноль!!!!')
+else:
+    print('Процесс успешно завершен')
+finally:
+    file.close()
+
+
+
