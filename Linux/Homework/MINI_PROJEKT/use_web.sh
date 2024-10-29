bbd044b75e19d6b7649ae0186c58237c18534d94
Mutaf-Volodymyr <mutaf.dev@gmail.com>
2024-10-29 18:11:15 +0100

    изменено:      Linux/Homework/MINI_PROJEKT/post_mortem.txt 	изменено:      Linux/Homework/MINI_PROJEKT/use_web.sh

--- Linux/Homework/MINI_PROJEKT/use_web.sh
+++ Linux/Homework/MINI_PROJEKT/use_web.sh
@@ -2,17 +2,15 @@
 
 address="3.73.44.38"
 fail_count=0
-while true
-do
-  ping_result=$(ping -c 1 "$address" 2>/dev/null)
 
-  if [[ $? -ne 0 ]]; then
-    ((fail_count++))
-    if [[ $fail_count -ge 10 ]]; then
-        # в этом блоке необходимо прописать отправку
-        # сообщения E-mail или SMS нужным людям
-    fi
-  else
-    fail_count=0
+ping_result=$(ping -c 1 "$address" 2>/dev/null)
+
+if [[ $? -ne 0 ]]; then
+  ((fail_count++))
+  if [[ $fail_count -ge 10 ]]; then
+      # в этом блоке необходимо прописать отправку
+      # сообщения E-mail или SMS нужным людям
   fi
-done
+else
+  fail_count=0
+fi
