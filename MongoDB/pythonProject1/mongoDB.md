d6549b678ac82a14cdf83300aaaaa561b8746753
Mutaf-Volodymyr <mutaf.dev@gmail.com>
2024-11-07 13:15:09 +0100

    изменено:      .DS_Store 	новый файл:    MongoDB/.DS_Store 	новый файл:    MongoDB/pythonProject1/.idea/.gitignore 	новый файл:    MongoDB/pythonProject1/.idea/dataSources.xml 	новый файл:    MongoDB/pythonProject1/.idea/git_toolbox_blame.xml 	новый файл:    MongoDB/pythonProject1/.idea/inspectionProfiles/profiles_settings.xml 	новый файл:    MongoDB/pythonProject1/.idea/misc.xml 	новый файл:    MongoDB/pythonProject1/.idea/modules.xml 	новый файл:    MongoDB/pythonProject1/.idea/pythonProject1.iml 	новый файл:    MongoDB/pythonProject1/.idea/vcs.xml 	новый файл:    MongoDB/pythonProject1/1.txt 	новый файл:    MongoDB/pythonProject1/Homework/HW_14 	новый файл:    MongoDB/pythonProject1/mongoDB.md 	изменено:      Python_fundamental/.DS_Store 	изменено:      Python_fundamental/Homework/.idea/workspace.xml 	новый файл:    Python_fundamental/Homework/HW_28.py 	изменено:      Python_fundamental/something/.DS_Store 	новый файл:    Python_fundamental/something/.idea/encodings.xml 	изменено:      Python_fundamental/something/2024_11_04.py 	новый файл:    Python_fundamental/something/__pycache__/findfiles.cpython-312.pyc 	новый файл:    Python_fundamental/something/anna-karenina_u8.txt 	изменено:      Stepik/.DS_Store 	новый файл:    Udemy/pythonProject/.idea/.gitignore 	новый файл:    Udemy/pythonProject/.idea/git_toolbox_blame.xml 	новый файл:    Udemy/pythonProject/.idea/inspectionProfiles/profiles_settings.xml 	новый файл:    Udemy/pythonProject/.idea/misc.xml 	новый файл:    Udemy/pythonProject/.idea/modules.xml 	новый файл:    Udemy/pythonProject/.idea/pythonProject.iml 	новый файл:    Udemy/pythonProject/.idea/vcs.xml 	новый файл:    Udemy/pythonProject/2024_11_04_oop.py

---
+++ MongoDB/pythonProject1/mongoDB.md
@@ -0,0 +1,17 @@
+####################### MongoDB ###################
+
+### Операторы
+- `$gte` - >=
+- `$lte` - <=
+- `$lt` - <
+- `$gt` - >
+- `$ne`- !=
+- `$in` - in 
+- `$or` - or
+- `$nin` - not in
+- `$and` - and
+- 
+
+
+https://github.com/pareikoVladislav/mongo/tree/main
+
--- Python_fundamental/something/2024_11_04.py
+++ Python_fundamental/something/2024_11_04.py
@@ -1,4 +1,64 @@
-from functools import reduce
-d = {23:1, 65:2, 45:56, 43:36}
-print(reduce(lambda x, y: x * y[1], d.items(), 0))
+# from string import punctuation
+#
+# with open('anna-karenina_u8.txt', 'r', encoding='utf-8') as f:
+#         text = f.read().split()
+#         text1 = filter(lambda y: len(y) > 10, map(lambda x: x.strip(punctuation+'«').lower(), text))
+#         print(*text1, sep='\n')
+
+
+# # 1
+# from functools import reduce
+# strings = ["apple", "banana", "cherry"]
+# print(sum(map(len, strings)))
+# print(reduce(lambda x, y: x + len(y), strings, 0))
+
+# 2 Создать новый список, содержащий суммы соответствующих элементов
+# list1 = [1, 2, 3, 4]
+# list2 = [5, 6, 7, 8]
+# print(list(map(sum, zip(list1, list2))))
+
+# 3 Конвертировать списка строк чтобы каждая строка начиналась с заглавной буквы,
+# а остальные буквы были маленькие (стРОкА -> Строка)
+# strings = ["стРОкА", "другаЯ стРОка", "последняя строкА"]
+# print(*map(str.title, strings))
+
+
+
+
+# numbers = [0, -1, 2, 3, -4, 5, -6, 7, 8, -9]
+# # возвести каждое положительное число в квадрат, найти их сумму
+# print(sum(map(lambda x:x**2, filter(lambda x:x>0, numbers))))
+
+
+# import itertools
+# import operator
+# numbers = [1, 2, 3, 4, 5]
+# result = itertools.accumulate(numbers, operator.mul)
+# print(result)
+
+#######################
+
+res = map(lambda x: x**2, filter(lambda x: x % 2 ==0, map(lambda x: x**2, map(int, input().split()))))
+print(*res)
+
+res = map(lambda x: x**4, filter(lambda x: x % 2 ==0, map(int, input().split())))
+print(*res)
+
+
+# Сгенерировать все пароли, состоящие из 4 символов чтобы среди них была хотя бы одна большая латинская буква, одна маленькая, одна цифра и никаких иных типов символов.
+# При этом запрещено, чтобы символ повторялся в пароле или две соседние по алфавиту буквы стояли рядом. Записать полученные пароли в файл.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
