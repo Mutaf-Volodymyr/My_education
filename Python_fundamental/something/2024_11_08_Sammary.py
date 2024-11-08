d00376418380c049c2f56246ca22e9efed832edd
Mutaf-Volodymyr <mutaf.dev@gmail.com>
2024-11-08 10:54:26 +0100

    изменено:      Python_fundamental/Homework/.idea/workspace.xml 	изменено:      Python_fundamental/Homework/HW_27.py 	новый файл:    Python_fundamental/something/2024_11_08_Sammary.py 	новый файл:    Python_fundamental/something/passwords.txt

---
+++ Python_fundamental/something/2024_11_08_Sammary.py
@@ -0,0 +1,27 @@
+# Сгенерировать все пароли, состоящие из 4 символов чтобы среди них была хотя бы одна
+# большая латинская буква, одна маленькая, одна цифра и никаких иных типов символов.
+# При этом запрещено, чтобы символ повторялся в пароле или две соседние по алфавиту буквы
+# стояли рядом. Записать полученные пароли в файл.
+
+from itertools import combinations
+from string import ascii_uppercase, ascii_lowercase, digits
+
+
+def is_valid_password(pas):
+    if pas == pas.upper() or pas == pas.lower():
+        return False
+    if not any(i.isdigit() for i in pas):
+        return False
+    for el1, el2 in zip(pas[:-1], pas[1:]):
+        if abs(ord(el1) - ord(el2)) < 2:
+            return False
+    return True
+
+with open('passwords.txt', 'w') as out:
+    res1 = combinations(ascii_uppercase + ascii_lowercase + digits, r=4)
+    res2= map(''.join, res1)
+    res3 = filter(is_valid_password, res2)
+    print(*res3, file=out)
+
+
+
