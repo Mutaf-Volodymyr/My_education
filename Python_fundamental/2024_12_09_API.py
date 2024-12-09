6a707f3ac17bec41b04a6cb7c2b3d46039d18357
Mutaf-Volodymyr <mutaf.dev@gmail.com>
2024-12-09 15:04:20 +0100

    изменено:      .DS_Store 	изменено:      My_certificates/.DS_Store 	новый файл:    Python_fundamental/111.html 	новый файл:    Python_fundamental/2024_12_09_API.py 	новый файл:    Python_fundamental/Homework/HW_35.py

---
+++ Python_fundamental/2024_12_09_API.py
@@ -0,0 +1,53 @@
+
+from re import findall
+
+import requests
+from bs4 import BeautifulSoup
+import re
+from collections import Counter
+
+
+
+
+
+
+
+# https://catfact.ninja/fact
+# response = requests.get('https://catfact.ninja/fact')
+# if response.status_code == 200:
+#     fact = response.json()['fact']
+#     print(fact)
+#     res = re.search(r'[^\w .,]', fact)
+#     if res is not None:
+#         print(False)
+#     else: print(True)
+
+
+
+# 11. Напишите программу, которая заменяет пробелы подчеркиваниями и обратно.
+# response = requests.get('https://catfact.ninja/fact')
+# if response.status_code == 200:
+#     fact = response.json()['fact']
+#     fact = re.sub(r' ', '_', fact)
+#     print(fact)
+#     fact = re.sub(r'_', ' ', fact)
+#     print(fact)
+
+
+# 12. Частью URL часто является дата в формате 2016/09/02. Например,
+# https://www.somesite.com/news/2024/01/22/article.html. Найдите все даты в URL такого вида.
+# Вывод может выглядеть так [('2024', '01', '22')].
+
+# text = 'https://www.somesite.com/news/2024/01/22/article.html'
+# pattern = r'\d{4}/\d{2}/\d{2}'
+# res = re.findall(pattern, text)
+# print(res)
+
+# 13. Напишите программу, которая конвертирует дату в формате yyyy-mm-dd в формат
+# dd-mm-yyyy.
+
+text = 'https://www.somesite.com/news/2024/01/22/article.html'
+# pattern = r'\d{4}/\d{2}/\d{2}'
+# res = re.findall(pattern, text)
+# print(res)
+
---
+++ Python_fundamental/Homework/HW_35.py
@@ -0,0 +1,47 @@
+# 1. Напишите функцию get_response(url), которая отправляет GET-запрос
+# по заданному URL-адресу и возвращает объект ответа. Затем выведите следующую информацию об ответе:
+# - Код состояния (status code)
+# - Текст ответа (response text)
+# - Заголовки ответа (response headers)
+
+# import requests
+# get_response = lambda url: requests.get(url)
+
+# url = "https://api.example.com"
+# url2 = "https://google.com"
+# response = get_response(url2)
+# print("Status Code:", response.status_code)
+# print("Response Text:", response.text)
+# print("Response Headers:", response.headers)
+
+
+
+
+# 2. Напишите функцию find_common_words(url_list), которая принимает список URL-адресов и возвращает
+# список наиболее часто встречающихся слов на веб-страницах. Для каждого URL-адреса функция должна
+# получить содержимое страницы с помощью запроса GET и использовать регулярные выражения для извлечения слов.
+# Затем функция должна подсчитать количество вхождений каждого слова и вернуть наиболее часто
+# встречающиеся слова в порядке убывания частоты.
+
+
+import requests
+from bs4 import BeautifulSoup
+import re
+from collections import Counter
+def find_common_words(url_list):
+    count = Counter()
+    for url in url_list:
+        response = requests.get(url)
+        words = BeautifulSoup (response.text, 'html.parser')
+        words = re.findall(r'\b[a-z]+\b', words.text, re.I)
+        count.update(map(str.lower, words))
+    return count
+
+urls = [
+    'https://google.com',
+    'https://stepik.org',
+    'https://github.com',
+    'https://www.bahn.de'
+]
+
+print(find_common_words(urls).most_common(10))
